eba9a4ce9af229c6e9b20690bc6350c2bd22c54e
Sasha <alecsand.unit@gmail.com>
2025-02-21 15:32:11 +0200

    dz1

--- SelectionFont/Class1.cs
+++ SelectionFont/Class1.cs
@@ -4,14 +4,16 @@ using System.Windows.Forms;
 
 namespace SelectionFont
 {
-    public class SelectionFontClass : ISelectionFontClass
+    public class SelectionFontClass : ISelectionFontClass 
     {
         public Font SelectFont()
         {
-            using (FontDialog fontDialog = new FontDialog())
+            using (FontDialog fontDialog = new FontDialog())    
             {
                 if (fontDialog.ShowDialog() == DialogResult.OK)
+                {
                     return fontDialog.Font;
+                }
             }
             return SystemFonts.DefaultFont;
         }
